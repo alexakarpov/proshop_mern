51cc7df2cf150fcbbdf6e52b8c9ec49240853c5b
Alexandre Karpov <alexakarpov@gmail.com>
2021-06-27 00:30:34 -0400

    frontend now using api for all current routes

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
-import products from '../products'
 import Product from '../components/Product'
+import axios from 'axios'
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([])
+
+  // runs on component load
+  useEffect(
+    () => {
+      const fetchProducts = async () => {
+        const { data } = await axios.get('/api/products')
+
+        setProducts(data)
+      }
+      fetchProducts()
+    }, // next is the array of dependencies
+    []
+  )
+
   return (
     <>
       <h1>Latest products</h1>
